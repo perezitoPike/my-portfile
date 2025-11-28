b190693c05596e3d61f7eefe3cb86dde3c7a2b19
perezitoPike <wilderchur@gmail.com>
2025-11-28 12:38:51 -0400

    Cuarto commit: Agregamos animaciones y eventos de bienvenida

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 console.log("Bienvenido");
 
-alert("Hola... Gracias por visitarme");
+//alert("Hola... Gracias por visitarme");
+
+let titleH1 = document.querySelector("h1");
+titleH1.addEventListener('click',()=>{
+    alert("Hola... Gracias por visitarme");
+});
